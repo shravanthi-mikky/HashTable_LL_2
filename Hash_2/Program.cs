c96653ed39d51e81eff4bc2f8b051137cdd428fb
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-16 12:31:12 +0530

    [shravanthi] - Frequency of words in para2

--- Hash_2/Program.cs
+++ Hash_2/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Hash Table Implementation using Linkedlist!");
 
 while(true)
 {
-    Console.WriteLine("Please choose the option :\n1) Frequency of para1");
+    Console.WriteLine("Please choose the option :\n1) Frequency of para1\n2) Frequency of para2");
     int option = Convert.ToInt16(Console.ReadLine());
     switch(option)
     {
@@ -36,7 +36,35 @@ while(true)
                 }
             }
             hash.Display();
-            break; 
+            break;
+        case 2:
+            HashMethods<string, int> hash2 = new HashMethods<string, int>(5);
+            string Words2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] array2 = Words2.Split(' ');
+            LinkedList<string> checkForDuplication2 = new LinkedList<string>();
+            foreach (string element in array2) 
+            {
+                int count = 0;
+                foreach (string match in array2)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplication2.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplication2.Contains(element))
+                {
+                    checkForDuplication2.AddLast(element);
+                    hash2.Add(element, count);
+                }
+            }
+            hash2.Display();
+            break;
+
         default:
             Console.WriteLine("Please choose the correct option!");
             break;
